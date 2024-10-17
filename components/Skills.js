728aa3b733a7da18536ecf4e9482a926201b7759
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-10-17 16:19:54 +0600

    clean up code

--- components/Skills.js
+++ components/Skills.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Skills = () => {
   return (
     <div className='w-full min-h-screen flex flex-col max-w-[1300px] mx-auto'>
-      <div className='w-full h-[800px] bg-gray-800 rounded-[3rem]'></div>
+      
     </div>
   )
 }
