0a6201a9a64b514ead13b730d40734f1904890c8
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-10-18 19:54:51 +0600

    finish styling footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,31 +7,31 @@ import { handleLinkClick } from './Hero'
 const Footer = () => {
   return (
     <div className='w-full px-2'>
-      <div className='w-full max-w-[1300px] min-h-[380px] flex flex-col items-center justify-between bg-[#1E1D1C] border border-zinc-800/50 mx-auto rounded-t-[2rem] py-16'>
+      <div className='w-full max-w-[1300px] min-h-[380px] flex flex-col gap-8 items-center justify-between bg-[#1E1D1C] border border-zinc-800/50 mx-auto rounded-t-[2rem] py-16 sm:px-6 px-4'>
         <h2 className='text-white text-center lg:text-3xl md:text-2xl sm:text-xl'>
         👋 Thanks for stopping by! Here is more of me if you are interested :{")"}
         </h2>
         <div className='w-full flex justify-center items-center'>
-          <div className='flex justify-between items-center w-[70%]'>
+          <div className='grid grid-cols-3 lg:gap-56 md:gap-24 sm:gap-8 gap-3'>
             {/* pages */}
-            <div className='flex flex-col gap-2'>
-              <p className='text-[#747265] uppercase text-sm'>Pages</p>
+            <div className='flex flex-col gap-2 justify-center'>
+              <p className='text-[#6a6c72] uppercase sm:text-sm text-xs'>Pages</p>
               {FOOTER_PAGES.map((link, index) => (
-                <Link href={link.href} key={index} className='text-[#7F8188] text-[18px] capitalize font-bold'>{link.label}</Link>
+                <Link href={link.href} key={index} className='text-[#7F8188] text-[18px] capitalize font-bold hover:text-white max-sm:text-sm'>{link.label}</Link>
               ))}
             </div>
             {/* selected work */}
-            <div className='flex flex-col gap-2'>
-              <p className='uppercase text-[#747265] text-sm'>Let&apos;s Connect</p>
+            <div className='flex flex-col gap-2 max-sm:text-sm'>
+              <p className='uppercase text-[#6a6c72] sm:text-sm text-xs'>Selected Work</p>
               {SELECTED_WORK.map((link, index) => (
-                <Link href={link.href} onClick={(e)=> handleLinkClick(e, "#work")} key={index} className='text-[#7F8188] text-[18px] capitalize font-bold'>{link.label}</Link>
+                <Link href={link.href} onClick={(e)=> handleLinkClick(e, '#work')} key={index} className='text-[#7F8188] text-[18px] capitalize font-bold hover:text-white max-sm:text-sm'>{link.label}</Link>
               ))}
             </div>
             {/* connect links */}
             <div className='flex flex-col gap-2'>
-              <p className='uppercase text-[#747265] text-sm'>Let&apos;s Connect</p>
+              <p className='uppercase text-[#6a6c72] sm:text-sm text-xs'>Let&apos;s Connect</p>
               {CONNECT_LINKS.map((link, index) => (
-                <a href={link.href} target='_blank' key={index} className='text-[#7F8188] text-[18px] capitalize font-bold'>{link.label}</a>
+                <a href={link.href} target='_blank' key={index} className='text-[#7F8188] text-[18px] capitalize font-bold hover:text-white max-sm:text-sm'>{link.label}</a>
               ))}
             </div>
           </div>
--- components/Work.js
+++ components/Work.js
@@ -4,12 +4,12 @@ import { project1, project2, project3, project4 } from '@/constants';
 const Work = () => {
   return (
     <section id="work" className="card-wrapper w-full flex flex-col items-center gap-40 relative lg:top-[70vh] top-[60vh] px-2">
-      <WorkCard id={'project1'} width={1200} top={84} position="sticky" title={project1.title} backgroundColor="#1E1D1C" description={project1.description} image={project1.image}/>
-      <WorkCard id={'project2'} width={1220} top={124} position="sticky" title={project2.title} backgroundColor="#1E1D1C" description={project2.description} image={project2.image}/>
-      <WorkCard id={'project3'} width={1240} top={164} position="sticky" title={project3.title} backgroundColor="#1E1D1C" description={project3.description} image={project3.image}/>
-      <WorkCard id={'project4'} width={1260} top={204} position="sticky" title={project4.title} backgroundColor="#1E1D1C" description={project4.description} image={project4.image}/>
+      <WorkCard width={1200} top={84} position="sticky" title={project1.title} backgroundColor="#1E1D1C" description={project1.description} image={project1.image}/>
+      <WorkCard width={1220} top={124} position="sticky" title={project2.title} backgroundColor="#1E1D1C" description={project2.description} image={project2.image}/>
+      <WorkCard width={1240} top={164} position="sticky" title={project3.title} backgroundColor="#1E1D1C" description={project3.description} image={project3.image}/>
+      <WorkCard width={1260} top={204} position="sticky" title={project4.title} backgroundColor="#1E1D1C" description={project4.description} image={project4.image}/>
     </section>
-  )
-}
+  );
+};
 
 export default Work;
