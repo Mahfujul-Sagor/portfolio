c609279a8f7e022682f96a8f7914cc092d9c81a6
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-10-21 19:06:58 +0600

    change padding and gaps in project card

--- components/WorkCard.js
+++ components/WorkCard.js
@@ -26,7 +26,7 @@ const WorkCard = ({
         <Image src={icon} alt="" width={100} height={100}/>
         <p className="text-3xl font-bold">{title}</p>
       </div>
-      <div className="flex max-md:flex-col-reverse justify-between items-center gap-5 w-full flex-1">
+      <div className="flex max-md:flex-col-reverse justify-between items-center gap-16 lg:gap-5 w-full flex-1">
         <div className="flex flex-col justify-center items-start gap-10 flex-1">
           <div className="space-y-4">
             <p className="text-[#7F8188] text-xl font-bold text-start">
