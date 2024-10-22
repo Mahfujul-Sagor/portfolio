7de005f97faf07e3a752f59bc1bcfbb5c5c15983
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-10-22 15:17:28 +0600

    add example background data

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { ABOUT_HEADER } from "@/constants";
+import Image from "next/image";
+import React from "react";
 
 const About = () => {
   return (
-    <div className='min-h-screen'>About</div>
-  )
-}
+    <div className="min-h-screen w-full mt-40">
+      <div className="text-center">
+        <h1 className="lg:text-7xl sm:text-5xl text-[32px] py-1 font-medium bg-clip-text text-transparent bg-gradient-to-b from-white to-[#7F8188]">
+          {ABOUT_HEADER.name}
+        </h1>
+        <h2 className="lg:text-4xl md:text-3xl sm:text-2xl text-[#7F8188]">
+          {ABOUT_HEADER.description}
+        </h2>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- constants/index.js
+++ constants/index.js
@@ -11,6 +11,18 @@ export const HERO = {
   name: "Mahfujul Sagor",
   description: "Building connections through every line of code.",
 };
+export const ABOUT_HEADER = {
+  name: "Mahfujul Sagor",
+  description: "Crafting meaningful digital experiences.",
+};
+
+export const BACKGROUND = {
+  title: "Background",
+  currentPosition: "I am currently a Human Interface Design Intern at Apple in Los Angeles, California, and an undergraduate student at the University of Maryland, College Park, pursuing dual degrees in Technology & Information Design (UX/UI) and Immersive Media Design (AR/VR).",
+  childhood: "Growing up, I was always passionate about three things. Creating digital designs, fashion, and playing soccer. Through my interest in design, I always wanted to use my skills to make others happy and enjoy what they were seeing visually. Through fashion, it allowed me to develop a natural eye for color, texture, and design. And through soccer, I learned that a lot of my skills translated to real world work as I gained valuable skills in leadership, communication, teamwork, and discipline. As I always wanted to grow in my design skills, fashion sense, and soccer abilities, it put me in a growth and curious mindset early on as I wanted to understand how to become better in everything I do.",
+  college: "During my freshman year, I discovered UX/UI Design as a perfect blend of being in design and making a meaningful impact in people’s lives through human centered design. Through my curiosity of learning UX before I took formal classes, I learned how to be empathetic to users, what design process will help you be successful, and having a deep understanding of how to solve problems creatively. And although my UX classes were very interesting, I knew that I wanted to look into the future of design in the XR space. With the addition of Immersive Media Design, it has made me learn how to approach design in a unique lens that many people would not consider and develop solutions that people wouldn’t even think about. Through UX/UI Design, I’ve learned how to help users in the 2D scene of mobile apps and websites. Through XR, I’ve learned to consider spatial awareness and audio and how that can revolutionize the way you design from not just designing interfaces, but the way we use everyday things around us. ",
+  conclusion: "With that being said, I hope my unique lens on how I approach design can not only make design more fun and interesting, but make the way we interact with technology become seamless with our everyday needs. ",
+}
 
 
 export const project1 = {
@@ -63,7 +75,6 @@ export const FOOTER_PAGES = [
 ]
 
 export const CONNECT_LINKS = [
-  { label: "Github", href: "https://github.com/Mahfujul-Sagor" },
   { label: "LinkedIn", href: "https://www.linkedin.com/in/mahfujul-sagor/" },
   { label: "X (formerly Twitter)", href: "https://x.com/Mahfujul_Sagor" },
   { label: "Instagram", href: "https://www.instagram.com/mahfujul.sagor/" },
